e43b5a528b3322781d8c2a6665ff2dbd761f2708
zamoroka <zapashok0@gmail.com>
2020-10-14 17:47:17 +0300

    Refactoring

---
+++ app/ClassFinder.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Zamoroka\MagentoDependencyGraph;
+
+class ClassFinder
+{
+    private $foldersToSkip = [
+        'Test',
+        'Tests',
+        'tests',
+        'Sniffs',
+        'PHP_CodeSniffer',
+        'Unit',
+        'MagentoHackathon',
+    ];
+
+    /**
+     * @param $dir
+     *
+     * @return array
+     */
+    public function getAllClasses($dir)
+    {
+        $classList = [];
+        foreach ($this->rSearch($dir, "/.*\.php$/") as $filename) {
+            try {
+                $classList[] = [
+                    'file_name' => $filename,
+                    'class_name' => $this->getClassFullNameFromFile($filename),
+                ];
+            } catch (\Exception $exception) {
+                // skip errors
+            }
+        }
+
+        return $classList;
+    }
+
+    /**
+     * @param $folder
+     * @param $pattern
+     *
+     * @return array
+     */
+    public function rSearch($folder, $pattern)
+    {
+        $fileList = [];
+        if (!is_dir($folder)) {
+            return $fileList;
+        }
+        $dir = new \RecursiveDirectoryIterator($folder);
+        $ite = new \RecursiveIteratorIterator($dir);
+        $files = new \RegexIterator($ite, $pattern, \RegexIterator::GET_MATCH);
+        foreach ($files as $file) {
+            $fileName = is_array($file) && isset($file[0]) ? $file[0] : (string)$file;
+            if ($this->isSkipFile($fileName)) {
+                continue;
+            }
+            $fileList[] = $fileName;
+        }
+
+        return $fileList;
+    }
+
+    /**
+     * @param $filePathName
+     *
+     * @return string
+     * @throws \Exception
+     */
+    public function getClassFullNameFromFile($filePathName)
+    {
+        $className = $this->getClassNameFromFile($filePathName);
+        if (!$className) {
+            throw new \Exception("not a class");
+        }
+
+        return $this->getClassNamespaceFromFile($filePathName) . '\\' . $this->getClassNameFromFile($filePathName);
+    }
+
+    /**
+     * @param $filePathName
+     *
+     * @return string|null
+     */
+    private function getClassNamespaceFromFile($filePathName)
+    {
+        $src = file_get_contents($filePathName);
+        $tokens = token_get_all($src);
+        $count = count($tokens);
+        $i = 0;
+        $namespace = '';
+        $namespaceOk = false;
+        while ($i < $count) {
+            $token = $tokens[$i];
+            if (is_array($token) && $token[0] === T_NAMESPACE) {
+                // Found namespace declaration
+                while (++$i < $count) {
+                    if ($tokens[$i] === ';') {
+                        $namespaceOk = true;
+                        $namespace = trim($namespace);
+                        break;
+                    }
+                    $namespace .= is_array($tokens[$i]) ? $tokens[$i][1] : $tokens[$i];
+                }
+                break;
+            }
+            $i++;
+        }
+        if (!$namespaceOk) {
+            return null;
+        } else {
+            return $namespace;
+        }
+    }
+
+    /**
+     * @param $filePathName
+     *
+     * @return mixed|null
+     */
+    private function getClassNameFromFile($filePathName)
+    {
+        $php_code = file_get_contents($filePathName);
+        $classes = array();
+        $tokens = token_get_all($php_code);
+        $count = count($tokens);
+        for ($i = 2; $i < $count; $i++) {
+            if ($tokens[$i - 2][0] == T_CLASS
+                && $tokens[$i - 1][0] == T_WHITESPACE
+                && $tokens[$i][0] == T_STRING
+            ) {
+
+                $className = $tokens[$i][1];
+                $classes[] = $className;
+            }
+        }
+
+        return isset($classes[0]) ? $classes[0] : null;
+    }
+
+    /**
+     * @param $fileName
+     *
+     * @return bool
+     */
+    private function isSkipFile($fileName)
+    {
+        $path = explode(DIRECTORY_SEPARATOR, $fileName);
+        foreach ($this->foldersToSkip as $needle) {
+            if (in_array($needle, $path)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- app/DependencyCollector.php
+++ app/DependencyCollector.php
@@ -11,30 +11,25 @@ class DependencyCollector
 {
     /** @var string */
     private $projectDir;
+    /** @var string|null */
+    private $moduleVendor;
+    /** @var ClassFinder */
+    private $classFinder;
     /** @var array */
     private $dependencies = [];
+    /** @var array */
+    private $skipped = [];
     /** @var array[] */
     private $modulesByLocation = [
         'app_code' => [],
         'vendor' => [],
     ];
-    private $foldersToSkip = [
-        'Test',
-        'Tests',
-        'tests',
-        'Sniffs',
-        'PHP_CodeSniffer',
-        'Unit',
-        'MagentoHackathon',
-    ];
-    private $skipped = [];
-    /** @var string|null */
-    private $moduleVendor;
 
     public function __construct(string $projectDir, string $moduleVendor = null)
     {
         $this->projectDir = $projectDir;
         $this->moduleVendor = $moduleVendor;
+        $this->classFinder = new ClassFinder();
     }
 
     /**
@@ -69,22 +64,6 @@ class DependencyCollector
         }
 
         return true;
-    }
-
-    /**
-     * @param $filePathName
-     *
-     * @return string
-     * @throws \Exception
-     */
-    public function getClassFullNameFromFile($filePathName)
-    {
-        $className = $this->getClassNameFromFile($filePathName);
-        if (!$className) {
-            throw new \Exception("not a class");
-        }
-
-        return $this->getClassNamespaceFromFile($filePathName) . '\\' . $this->getClassNameFromFile($filePathName);
     }
 
     /**
@@ -132,67 +111,6 @@ class DependencyCollector
     }
 
     /**
-     * @param $filePathName
-     *
-     * @return string|null
-     */
-    private function getClassNamespaceFromFile($filePathName)
-    {
-        $src = file_get_contents($filePathName);
-        $tokens = token_get_all($src);
-        $count = count($tokens);
-        $i = 0;
-        $namespace = '';
-        $namespace_ok = false;
-        while ($i < $count) {
-            $token = $tokens[$i];
-            if (is_array($token) && $token[0] === T_NAMESPACE) {
-                // Found namespace declaration
-                while (++$i < $count) {
-                    if ($tokens[$i] === ';') {
-                        $namespace_ok = true;
-                        $namespace = trim($namespace);
-                        break;
-                    }
-                    $namespace .= is_array($tokens[$i]) ? $tokens[$i][1] : $tokens[$i];
-                }
-                break;
-            }
-            $i++;
-        }
-        if (!$namespace_ok) {
-            return null;
-        } else {
-            return $namespace;
-        }
-    }
-
-    /**
-     * @param $filePathName
-     *
-     * @return mixed|null
-     */
-    private function getClassNameFromFile($filePathName)
-    {
-        $php_code = file_get_contents($filePathName);
-        $classes = array();
-        $tokens = token_get_all($php_code);
-        $count = count($tokens);
-        for ($i = 2; $i < $count; $i++) {
-            if ($tokens[$i - 2][0] == T_CLASS
-                && $tokens[$i - 1][0] == T_WHITESPACE
-                && $tokens[$i][0] == T_STRING
-            ) {
-
-                $class_name = $tokens[$i][1];
-                $classes[] = $class_name;
-            }
-        }
-
-        return isset($classes[0]) ? $classes[0] : null;
-    }
-
-    /**
      * @param string $dir
      *
      * @return array
@@ -204,10 +122,10 @@ class DependencyCollector
         $classLoader = require $this->projectDir . 'vendor/autoload.php';
         $astLocator = (new BetterReflection())->astLocator();
         $reflector = new ClassReflector(new ComposerSourceLocator($classLoader, $astLocator));
-        foreach ($this->rSearch($dir, "/.*\.php$/") as $filename) {
+        $classes = $this->classFinder->getAllClasses($dir);
+        foreach ($classes as $class) {
             try {
-                $className = $this->getClassFullNameFromFile($filename);
-                $classInfo = $reflector->reflect($className);
+                $classInfo = $reflector->reflect($class['class_name']);
                 //        --- current module ---
                 $currentModule = $this->getModuleName($classInfo);
                 if (!$currentModule) {
@@ -235,11 +153,11 @@ class DependencyCollector
                 //        --- unique dependencies ---
                 $collect[$currentModule] = array_unique($collect[$currentModule]);
             } catch (\Exception $exception) {
-                $filename = str_replace($dir . '/', '', $filename);
-                $this->skipped[] = $filename;
+                $this->skipped[] = $class;
             }
         }
-        foreach ($this->rSearch($dir, "/.*module.xml/") as $filename) {
+        //        --- module.xml dependencies ---
+        foreach ($this->classFinder->rSearch($dir, "/.*module.xml/") as $filename) {
             $xml = file_get_contents($filename);
             $dom = new \DOMDocument();
             $dom->loadXML($xml);
@@ -277,7 +195,6 @@ class DependencyCollector
         if ($this->moduleVendor && strpos($module, $this->moduleVendor) === false) {
             return $this;
         }
-
         if (!isset($this->dependencies[$module])) {
             $this->dependencies[$module] = [];
         }
@@ -328,49 +245,6 @@ class DependencyCollector
         }
 
         return $modules;
-    }
-
-    /**
-     * @param $folder
-     * @param $pattern
-     *
-     * @return array
-     */
-    private function rSearch($folder, $pattern)
-    {
-        $fileList = [];
-        if (!is_dir($folder)) {
-            return $fileList;
-        }
-        $dir = new \RecursiveDirectoryIterator($folder);
-        $ite = new \RecursiveIteratorIterator($dir);
-        $files = new \RegexIterator($ite, $pattern, \RegexIterator::GET_MATCH);
-        foreach ($files as $file) {
-            $fileName = is_array($file) && isset($file[0]) ? $file[0] : (string)$file;
-            if ($this->isSkipFile($fileName)) {
-                continue;
-            }
-            $fileList[] = $fileName;
-        }
-
-        return $fileList;
-    }
-
-    /**
-     * @param $fileName
-     *
-     * @return bool
-     */
-    private function isSkipFile($fileName)
-    {
-        $path = explode(DIRECTORY_SEPARATOR, $fileName);
-        foreach ($this->foldersToSkip as $needle) {
-            if (in_array($needle, $path)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     /**
